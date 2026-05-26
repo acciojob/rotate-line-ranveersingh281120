a9ae9c976f9c77916d5481ba926c8bd2573ca2b9
ranveersingh281120 <ranveersingh281120@gmail.com>
2026-05-26 13:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let deg = 0;
+
+setInterval( function () {
+
+	line.style.transform = `rotate(${deg}deg)`;
+	deg +=2;
+	
+	
+},20)
